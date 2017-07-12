be4fa04307894923712031c2684e864636d2a175
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2017-07-12 15:41:08 +0200

    Rely more on date methods. Add "shiftStartDate" and shiftStopDate" functions. Add "isBefore" and "isAfter" functions.

--- timeframe.js
+++ timeframe.js
@@ -1,14 +1,17 @@
 'use strict';
 
 function Timeframe(startDate, stopDate) {
-	if(startDate && stopDate && stopDate.getTime() < startDate.getTime()) {
+	if(startDate && stopDate && stopDate.isBefore(startDate)) {
 		throw new Error('Unable to create a timeframe with a stop date before its start date');
 	}
 	this.startDate = startDate;
 	this.stopDate = stopDate;
 }
+Timeframe.prototype.isInfinite = function() {
+	return !this.startDate && !this.stopDate;
+}
 Timeframe.prototype.isStaked = function() {
-	return this.startDate && this.stopDate;
+	return !!(this.startDate && this.stopDate);
 };
 Timeframe.prototype.getDays = function() {
 	return this.isStaked() ? Date.getDifferenceInDays(this.startDate, this.stopDate) : undefined;
@@ -27,20 +30,33 @@ Timeframe.prototype.clone = function() {
 	return new Timeframe(this.startDate ? this.startDate.clone() : undefined, this.stopDate ? this.stopDate.clone() : undefined);
 };
 Timeframe.prototype.surround = function(date) {
-	return (!this.startDate || this.startDate <= date) && (!this.stopDate || this.stopDate >= date);
+	return (!this.startDate || this.startDate.isBefore(date) || this.startDate.equals(date)) && (!this.stopDate || this.stopDate.isAfter(date) || this.stopDate.equals(date));
 };
 Timeframe.prototype.overlap = function(timeframe) {
-	return this.surround(timeframe.startDate) || this.surround(timeframe.stopDate) || timeframe.surround(this.startDate) || timeframe.surround(this.stopDate);
+	if(timeframe.isInfinite()) {
+		return true;
+	}
+	if(!timeframe.startDate) {
+		return !this.startDate || this.startDate.isBefore(timeframe.stopDate);
+	}
+	if(!timeframe.stopDate) {
+		return !this.stopDate || this.stopDate.isBefore(timeframe.startDate);
+	}
+	return this.surround(timeframe.startDate) || this.surround(timeframe.stopDate) || timeframe.surround(this.startDate);
 };
 Timeframe.prototype.toString = function() {
 	return this.startDate + ' - ' + this.stopDate;
 };
 Timeframe.prototype.equals = function(timeframe) {
+	if(!timeframe) {
+		return false;
+	}
 	var same_start_date = !this.startDate && !timeframe.startDate || this.startDate && timeframe.startDate && this.startDate.getTime() === timeframe.startDate.getTime();
 	var same_stop_date = !this.stopDate && !timeframe.stopDate || this.stopDate && timeframe.stopDate && this.stopDate.getTime() === timeframe.stopDate.getTime();
 	return same_start_date && same_stop_date;
 };
 
+//TODO take infinite timeframes in consideration
 Timeframe.prototype.extendPercentage = function(percentage) {
 	var margin = Math.floor(this.getSeconds() * percentage / 100);
 	this.startDate.addSeconds(-margin);
@@ -89,22 +105,6 @@ Timeframe.prototype.roundToMinute = function() {
 	return this;
 };
 
-Timeframe.prototype.shiftHours = function(hours) {
-	this.startDate.addHours(hours);
-	this.stopDate.addHours(hours);
-	return this;
-};
-Timeframe.prototype.shiftMinutes = function(minutes) {
-	this.startDate.addMinutes(minutes);
-	this.stopDate.addMinutes(minutes);
-	return this;
-};
-Timeframe.prototype.shiftSeconds = function(seconds) {
-	this.startDate.addSeconds(seconds);
-	this.stopDate.addSeconds(seconds);
-	return this;
-};
-
 Timeframe.prototype.shiftDays = function(days) {
 	this.startDate.addDays(days);
 	this.stopDate.addDays(days);
@@ -124,4 +124,20 @@ Timeframe.prototype.shiftSeconds = function(seconds) {
 	this.startDate.addSeconds(seconds);
 	this.stopDate.addSeconds(seconds);
 	return this;
+};
+
+Timeframe.prototype.shiftStartDate = function(date) {
+	var offset = date.getTime() - this.startDate.getTime();
+	return this.shiftSeconds(offset / 1000);
+};
+Timeframe.prototype.shiftStopDate = function(date) {
+	var offset = date.getTime() - this.stopDate.getTime();
+	return this.shiftSeconds(offset / 1000);
+};
+
+Timeframe.prototype.isBefore = function(timeframe) {
+	return this.startDate.isBefore(timeframe.startDate);
+};
+Timeframe.prototype.isAfter = function(timeframe) {
+	return this.stopDate.isAfter(timeframe.stopDate);
 };
--- timeframe.test.js
+++ timeframe.test.js
@@ -2,7 +2,25 @@
 
 assert.begin();
 
+assert.doesThrow(
+	function() {
+		new Timeframe(new Date('2004/02/26'), new Date('2004/02/25'));
+	},
+	undefined,
+	'It is not possible to create a timeframe with a stop date before its start date'
+);
+
 var timeframe;
+
+//infinite
+assert.notOk(new Timeframe(new Date('2004/02/26'), new Date('2004/02/27')).isInfinite(), 'Timeframe with start date and stop date is not infinite');
+assert.notOk(new Timeframe(new Date('2004/02/26')).isInfinite(), 'Timeframe with only start date is not infinite');
+assert.ok(new Timeframe().isInfinite(), 'Timeframe without start date nor stop date is infinite');
+
+//staked
+assert.ok(new Timeframe(new Date('2004/02/26'), new Date('2004/02/27')).isStaked(), 'Timeframe with start date and stop date is stacked');
+assert.notOk(new Timeframe(new Date('2004/02/26')).isStaked(), 'Timeframe with only start date is not stacked');
+assert.notOk(new Timeframe().isStaked(), 'Timeframe without start date nor stop date is not stacked');
 
 //periods
 timeframe = new Timeframe(new Date('2004/02/26'), new Date('2004/02/27'));
@@ -20,13 +38,24 @@ assert.equal(timeframe.getSeconds(), 0, 'There is 0 second between [2010/12/31] 
 timeframe = new Timeframe(new Date('2004/02/26'), new Date('2004/03/01'));
 assert.equal(timeframe.getDays(), 4, 'There are 4 days between [2004/02/26] and [2004/03/01]');
 
-assert.doesThrow(
-	function() {
-		new Timeframe(new Date('2004/02/26'), new Date('2004/02/25'));
-	},
-	undefined,
-	'It is not possible to create a timeframe with a stop date before its start date'
-);
+timeframe = new Timeframe(new Date('2016-05-01T08:12:04.999Z'), new Date('2016-05-01T08:12:08.001Z'));
+assert.equal(Math.round(timeframe.getDays()), 0, 'There are 0 day between [2016-05-01T08:12:04.999Z] and [2016-05-01T08:12:08.001Z]');
+assert.equal(Math.round(timeframe.getHours()), 0, 'There are 0 hour between [2016-05-01T08:12:04.999Z] and [2016-05-01T08:12:08.001Z]');
+assert.equal(Math.round(timeframe.getMinutes()), 0, 'There are 0 minute between [2016-05-01T08:12:04.999Z] and [2016-05-01T08:12:08.001Z]');
+assert.equal(timeframe.getSeconds(), 3.002, 'There are 3.002 seconds between [2016-05-01T08:12:04.999Z] and [2016-05-01T08:12:08.001Z]');
+
+timeframe = new Timeframe(undefined, new Date('2016-05-01T08:12:04.999Z'));
+assert.undefined(timeframe.getDays(), 'Retrieving number of days with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getHours(), 'Retrieving number of hours with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getMinutes(), 'Retrieving number of minutes with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getSeconds(), 'Retrieving number of seconds with an unstaked timeframe returns undefined');
+
+//infinite time frame periods
+timeframe = new Timeframe(new Date('2004/02/26'));
+assert.undefined(timeframe.getDays(), 'Retrieving number of days with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getHours(), 'Retrieving number of hours with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getMinutes(), 'Retrieving number of minutes with an unstaked timeframe returns undefined');
+assert.undefined(timeframe.getSeconds(), 'Retrieving number of seconds with an unstaked timeframe returns undefined');
 
 //shift
 timeframe = new Timeframe(new Date('2010/12/27'), new Date('2010/12/31'));
@@ -39,8 +68,19 @@ assert.equal(timeframe.stopDate.toDisplay(), '07.01.2011', 'Timeframe ending [20
 //surround
 timeframe = new Timeframe(new Date('2009/02/01'), new Date('2009/10/01'));
 assert.ok(timeframe.surround(new Date('2009/04/01')), '2009/04/01 is between 2009/02/01 and 2009/10/01');
-assert.ok(timeframe.surround(new Date('2009/08/01')), '2009/08/01 is between 2009/02/01 and 2009/10/01');
+assert.ok(timeframe.surround(new Date('2009/10/01')), '2009/10/01 is between 2009/02/01 and 2009/10/01');
 assert.notOk(timeframe.surround(new Date('2009/01/25')), '2009/01/25 is not between 2009/02/01 and 2009/10/01');
+
+//infinite time frame surround
+var timeframe = new Timeframe(new Date('2009/02/01'));
+assert.ok(timeframe.surround(new Date('2009/04/01')), '2009/04/01 is between 2009/02/01 and infinite');
+assert.ok(timeframe.surround(new Date('2200/08/01')), '2200/08/01 is between 2009/02/01 and infinite');
+assert.notOk(timeframe.surround(new Date('2009/01/25')), '2009/01/25 is not between 2009/02/01 and infinite');
+
+var timeframe = new Timeframe(undefined, new Date('2009/02/01'));
+assert.ok(timeframe.surround(new Date('2009/01/25')), '2009/01/25 is between infinite and 2009/02/01');
+assert.ok(timeframe.surround(new Date('1800/08/01')), '1800/08/01 is between infinite and 2009/02/01');
+assert.notOk(timeframe.surround(new Date('2009/04/01')), '2009/04/01 is not between infinite and 2009/02/01');
 
 //overlap
 var timeframe_1 = new Timeframe(new Date('2009/02/01'), new Date('2009/10/01'));
@@ -55,32 +95,6 @@ timeframe_1 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
 timeframe_2 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
 assert.ok(timeframe_1.overlap(timeframe_2), 'Timeframe [2008/02/07 to 2008/02/07] overlaps timeframe [2008/02/07 to 2008/02/07]');
 
-//equal
-timeframe_1 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
-timeframe_2 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
-assert.similar(timeframe_1, timeframe_2, 'Two timeframes with same start dates and same stop dates are similar');
-
-timeframe_2 = new Timeframe(new Date('2008/02/07'), new Date('2008/08/07'));
-assert.notSimilar(timeframe_1, timeframe_2, 'Two timeframes with same start dates and different stop dates are not similar');
-
-//infinite time frame periods
-timeframe = new Timeframe(new Date('2004/02/26'));
-assert.undefined(timeframe.getDays(), 'Retrieving number of days with an unstaked timeframe returns undefined');
-assert.undefined(timeframe.getHours(), 'Retrieving number of hours with an unstaked timeframe returns undefined');
-assert.undefined(timeframe.getMinutes(), 'Retrieving number of minutes with an unstaked timeframe returns undefined');
-assert.undefined(timeframe.getSeconds(), 'Retrieving number of seconds with an unstaked timeframe returns undefined');
-
-//infinite time frame surround
-var timeframe = new Timeframe(new Date('2009/02/01'));
-assert.ok(timeframe.surround(new Date('2009/04/01')), '2009/04/01 is between 2009/02/01 and infinite');
-assert.ok(timeframe.surround(new Date('2200/08/01')), '2200/08/01 is between 2009/02/01 and infinite');
-assert.notOk(timeframe.surround(new Date('2009/01/25')), '2009/01/25 is not between 2009/02/01 and infinite');
-
-var timeframe = new Timeframe(undefined, new Date('2009/02/01'));
-assert.ok(timeframe.surround(new Date('2009/01/25')), '2009/01/25 is between infinite and 2009/02/01');
-assert.ok(timeframe.surround(new Date('1800/08/01')), '1800/08/01 is between infinite and 2009/02/01');
-assert.notOk(timeframe.surround(new Date('2009/04/01')), '2009/04/01 is not between infinite and 2009/02/01');
-
 //infinite time frame overlap
 var timeframe_1 = new Timeframe(new Date('2009/02/01'));
 var timeframe_2 = new Timeframe(new Date('2009/04/01'), new Date('2009/08/01'));
@@ -89,6 +103,14 @@ assert.ok(timeframe_1.overlap(timeframe_2), 'Timeframe [2009/04/01 to infinite] 
 timeframe_1 = new Timeframe(undefined, new Date('2008/09/07'));
 timeframe_2 = new Timeframe(undefined, new Date('2010/02/20'));
 assert.ok(timeframe_1.overlap(timeframe_2), 'Timeframe [infinite to 2008/09/07] overlaps timeframe [infinite to 2010/02/20]');
+
+//equal
+timeframe_1 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
+timeframe_2 = new Timeframe(new Date('2008/02/07'), new Date('2008/09/07'));
+assert.similar(timeframe_1, timeframe_2, 'Two timeframes with same start dates and same stop dates are similar');
+
+timeframe_2 = new Timeframe(new Date('2008/02/07'), new Date('2008/08/07'));
+assert.notSimilar(timeframe_1, timeframe_2, 'Two timeframes with same start dates and different stop dates are not similar');
 
 //infinite time frame equal
 timeframe_1 = new Timeframe(new Date('2008/02/07'));
