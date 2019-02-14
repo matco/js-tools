c6b54fdbfff86257f7972f7e49b43928cba982e8
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2019-02-14 17:03:17 +0100

    Improve report to make it match Junit. Improve code.

--- suite.js
+++ suite.js
@@ -29,7 +29,7 @@ export class Suite {
 			hostname: window.navigator.userAgent,
 			failures: this.counters.fails,
 			tests: this.getTests(),
-			duration: this.getDuration() / 1000,
+			time: this.getDuration() / 1000,
 			timestamp: this.times.start.toISOString()
 		});
 		report.documentElement.appendChild(test_suite);
@@ -39,7 +39,7 @@ export class Suite {
 		properties.appendChild(report.createFullElement('property', {name: 'platform', value: window.navigator.platform}));
 		properties.appendChild(report.createFullElement('property', {name: 'os.name', value: window.navigator.oscpu}));
 		//add test cases
-		Array.prototype.concat.apply([], this.bundles.map(b => b.assert.results)).map(function(result) {
+		this.bundles.flatMap(b => b.assert.results).map(function(result) {
 			const test_case = report.createFullElement('testcase', {name: result.message || '', specification: result.specification || ''});
 			if(!result.success) {
 				test_case.appendChild(report.createFullElement('failure', {message: result.message}));
@@ -50,7 +50,7 @@ export class Suite {
 	run(doc) {
 		this.times.start = new Date();
 		const that = this;
-		return that.bundles.reduce((a, b) => {return a.then(Bundle.prototype.run.bind(b, doc));}, Promise.resolve()).then(function() {
+		return that.bundles.reduce((a, b) => a.then(Bundle.prototype.run.bind(b, doc)), Promise.resolve()).then(function() {
 			that.times.stop = new Date();
 			return that;
 		});
@@ -59,9 +59,7 @@ export class Suite {
 		//create suite
 		const suite = new Suite(s.name, s.path);
 		//create bundles
-		suite.bundles = s.bundles.map(function(b) {
-			return new Bundle(suite, b.native, b.dom, b.website, b.dependencies, b.test);
-		});
+		suite.bundles = s.bundles.map(b => new Bundle(suite, b.native, b.dom, b.website, b.dependencies, b.test));
 		return suite;
 	}
 	static generateReport(name, suites, stylesheet_url) {
@@ -77,7 +75,7 @@ export class Suite {
 		report.documentElement.setAttribute('name', name);
 		report.documentElement.setAttribute('failures', suites.map(s => s.counters.fails).reduce((previous, currrent) => previous + currrent));
 		report.documentElement.setAttribute('tests', suites.map(s => s.getTests()).reduce((previous, currrent) => previous + currrent));
-		report.documentElement.setAttribute('duration', suites.map(s => s.getDuration()).reduce((previous, currrent) => previous + currrent) / 1000);
+		report.documentElement.setAttribute('time', suites.map(s => s.getDuration()).reduce((previous, currrent) => previous + currrent) / 1000);
 		suites.map(s => s.contributeToReport(report));
 		return report;
 	}
