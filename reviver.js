c9e47e1f885835cd4890f0de90235d04f72c4ea2
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2021-03-16 15:29:57 +0100

    Rewrite reviver: -allow to check for property values -add JSDoc

--- reviver.js
+++ reviver.js
@@ -1,25 +1,54 @@
+class TypeDoesNotMatch extends Error {
+	constructor(container, value, type) {
+		super(`Value ${value} does not match the expected type "${type}" in ${container.constructor.name}`);
+	}
+}
+
+/**
+ * @typedef {Function} Factory
+ * @param {string} entity - String containing the object entity
+ * @param {string} [container] - The container of the object that is currently being revived
+ * @returns {Function} - The constructor associated to the entity
+ */
+
 class Reviver {
+	static ENTITY_PROPERTY = 'className';
+
 	constructor(parameters) {
-		//String : property used to know how to type raw objects
+		//String: property used to know which property of plain objects must be used to determine their entity
 		this.entityProperty = Reviver.ENTITY_PROPERTY;
-		//String : value used to know which property is a back reference
-		this.backReferenceValue = Reviver.BACK_REFERENCE;
-		//Function : must return an instance for entity
-		//you create object manually
+		//factory used to create objects if you want to create objects manually
+		//Function: return an instance for entity
+		//entity, container => an instance of the class matching the parameter entity
+		/**@type {Factory} */
 		this.factory;
-		//Function : must return a constructor for an entity
+		//delegate creation of the objects to the reviver, only providing a constructor function
+		//Function: return a constructor for an entity that will be used to create the object
+		//entity, plain_object => a function that will be used as a constructor
 		this.entitiesConstructors = undefined;
-		//Function : must return an array of all properties for an entity
-		//only properties returned by this function are imported
+		//specify properties that can be imported can be left undefined
+		//if specified, only properties returned by this function are imported
+		//Function: must return a map of all properties for an entity
+		//entity => map of entities properties
+		//the keys of the map must be the name of the properties and the values a property object
+		//a property object can have following fields:
+		//type: the type of the property (string, number, boolean, array or object or the id of an entity)
+		//back_reference: a boolean specifying that this property must be set to the container
 		this.entitiesProperties = undefined;
-		//Boolean : preserve entity property
-		//entity property must be returned by this.entitiesProperties function to be kept in final object in strict mode
+		//Boolean: do not import a property that does not match its type definition
+		//this works only if properties have a type
+		this.enforceTypes = false;
+		//Boolean: preserve unknown properties
+		//keep properties that are not returned by the "entitiesProperties" function
+		this.preserveUnknownProperties = false;
+		//Boolean: preserve entity property
+		//"preserveUnknownProperties" works even if the "entityProperty" is not returned by the "entitiesProperties" function
 		this.preserveEntityProperty = false;
-		//Boolean : write to console properties which are present in raw object but have not been defined by this.entitiesProperties function
-		//show something only in strict mode
+		//Boolean: write to console properties which are present in plain object but have not been defined by this.entitiesProperties function
+		//show something only when "preserveUnknownProperties" is set to false
 		this.debug = false;
-		//Function : callback to apply for every objects after the revival
-		//callback is called after the revival with four arguments : revived object, entity, container and raw object
+		//Function: callback to apply for every objects after the revival
+		//() => revived_object, entity, container, plain_object
 		this.callback = undefined;
 
 		//bind parameters
@@ -31,100 +60,118 @@ class Reviver {
 		if(!this.factory && !this.entitiesConstructors) {
 			throw 'A factory or a function which returns entity constructor is required';
 		}
-		if(this.factory && this.entitiesConstructors) {
-			throw 'You can not specify a factory and an entity constructor function at the same time';
-		}
 
 		//build factory using constructor function
 		if(!this.factory && this.entitiesConstructors) {
 			this.factory = function(entity) {
 				const builder = this.entitiesConstructors(entity);
 				if(!builder) {
-					throw new Error('Missing constructor for entity ' + entity);
+					throw new Error(`Missing constructor for entity ${entity}`);
 				}
 				return new builder();
 			};
 		}
 	}
-	revive(object, container) {
+	/**
+	 * @param {object} object - The object to revive
+	 * @param {any} [container] - The container of the object that will be bound to object back references
+	 * @param {string} [type] - Awaited type of object
+	 * @returns {any} - The revived object
+	 */
+	revive(object, container, type) {
 		//creating new entity
-		if(object) {
+		if(object !== undefined && object !== null) {
 			//array of objects
 			if(Array.isArray(object)) {
-				//following code is equivalent to "return object.map(o => this.revive(o, container));" but faster!
-				const proto_object = [];
-				for(let i = 0, length = object.length; i < length; i++) {
-					proto_object[i] = this.revive(object[i], container);
+				if(this.enforceTypes && type && type !== 'array') {
+					throw new TypeDoesNotMatch(container, object, type);
 				}
-				return proto_object;
+				return object.map(o => this.revive(o, container));
 			}
 			//object
 			if(typeof object === 'object') {
 				//typed object
 				if(object[this.entityProperty]) {
-					//create prototyped object
-					//TODO improve this, it would be nice to send values directly to factory to be able to construct the object directly with its values
-					//this would open the doors for immutables objects
-					//this requires to set back reference in a second pass
-					const proto_object = this.factory(object[this.entityProperty]);
-					//import properties
-					for(const property in object) {
-						//check current property is not inherited and not the entity property (if entity property is not preserved)
-						if(object.hasOwnProperty(property) && (this.preserveEntityProperty || property !== this.entityProperty)) {
-							//in strict mode, check that current property has been declared in class
-							if(!this.entitiesProperties || this.entitiesProperties(object[this.entityProperty]).includes(property)) {
-								proto_object[property] = this.revive(object[property], proto_object);
-							}
-							else {
-								//delete property
-								delete proto_object[property];
-								if(this.debug) {
-									//warn user that a property from the object has not been declared in class
-									console.log('Property ' + property + ' (value: ' + object[property] + ') does not exist in ' + object[this.entityProperty] + ' and has been deleted', 'Object:', object);
+					//create revived object
+					//send object values directly to the factory to be able to construct the object directly with its values
+					//this allows for immutables objects
+					const revived_object = this.factory(object[this.entityProperty], object);
+					//retrieve properties
+					const declared_properties = this.entitiesProperties ? this.entitiesProperties(object[this.entityProperty]) : undefined;
+					//import properties, looping only on own properties (property must no be inherited)
+					for(const [property, value] of Object.entries(object)) {
+						//check that current property has been declared or unknown properties are preserved or this is the entity property
+						if(this.preserveUnknownProperties || this.preserveEntityProperty && property === this.entityProperty || declared_properties.hasOwnProperty(property)) {
+							//revive may fail du to incompatible types
+							revived_object[property] = this.revive(value, revived_object, declared_properties[property]?.type);
+						}
+						else if(this.debug && !declared_properties.hasOwnProperty(property)) {
+							//warn user that a property from the object has not been declared in class
+							console.log(`Property ${property} (value: ${value}) does not exist in ${object[this.entityProperty]} and has not been assigned for object`, object);
+						}
+					}
+					//set back references only available when entities properties are provided
+					if(declared_properties && container) {
+						//there can be more than one back reference for entities linked to two different parent entities
+						const back_references = Object.entries(declared_properties).filter(e => e[1].back_reference);
+						if(back_references.length > 0) {
+							let back_reference_set = false;
+							for(const [name, definition] of back_references) {
+								//check if back reference match property type
+								//the goal is to associate the container to the right back reference
+								//some entities may have more than one reference if they are used in different containers
+								//this is only possible if "entitiesConstructors" is used
+								if(!this.entitiesConstructors) {
+									back_reference_set = true;
+									revived_object[name] = container;
+								}
+								else {
+									//if type is specified, set back reference only if type of container matches declared type
+									if(definition.type) {
+										if(definition.type === container.constructor.name) {
+											back_reference_set = true;
+											revived_object[name] = container;
+										}
+									}
+									else {
+										back_reference_set = true;
+										revived_object[name] = container;
+									}
 								}
 							}
-						}
-					}
-					//set back references
-					if(container) {
-						const properties = Object.getOwnPropertyNames(proto_object);
-						//inverse loop for performance reasons
-						for(let i = properties.length - 1; i >= 0; i--) {
-							const property = properties[i];
-							if(proto_object[property] === this.backReferenceValue) {
-								proto_object[property] = container;
-								//exclude this property for enumeration
-								/*var descriptor = Object.getOwnPropertyDescriptor(proto_object, property);
-								descriptor.enumerable = false;
-								Object.defineProperty(proto_object, property, descriptor);*/
-								//break; //only one back reference allowed //wrong, there can be more than one back references for entities linked to two different other entities
+							if(this.debug && !back_reference_set) {
+								//warn user that an object has back references but none could be set
+								console.info('No back reference have been set for object', object);
 							}
 						}
+						//exclude this property for enumeration
+						/*var descriptor = Object.getOwnPropertyDescriptor(proto_object, property);
+						descriptor.enumerable = false;
+						Object.defineProperty(proto_object, property, descriptor);*/
 					}
 					if(this.callback) {
-						this.callback(proto_object, object[this.entityProperty], container, object);
+						this.callback(revived_object, object[this.entityProperty], container, object);
 					}
-					return proto_object;
+					return revived_object;
 				}
-				//map
+				//map of objects
 				else {
 					const proto_object = {};
-					for(const key in object) {
-						if(object.hasOwnProperty(key)) {
-							//revive each value keeping a reference to container
-							proto_object[key] = this.revive(object[key], container);
-						}
+					for(const [key, value] of Object.entries(object)) {
+						//revive each value keeping a reference to container
+						proto_object[key] = this.revive(value, container);
 					}
 					return proto_object;
 				}
 			}
+			//primitive data type
+			if(this.enforceTypes && type && typeof(object) !== type) {
+				throw new TypeDoesNotMatch(container, object, type);
+			}
 		}
-		//object is not valid or is a primitive data type, return the original object without modification
+		//object is undefined, nothing to do on it
 		return object;
 	}
 }
-
-Reviver.BACK_REFERENCE = 'BACK_REFERENCE';
-Reviver.ENTITY_PROPERTY = 'entity';
 
 export {Reviver};
--- reviver.test.js
+++ reviver.test.js
@@ -1,80 +1,104 @@
 import {Reviver} from './reviver.js';
 
-const TestEntities = {
-	City: function() {
-		this.names = {};
-		this.population = 10;
-		this.streets = [];
-
-		this.getLongStreets = function() {
-			const streets = [];
-			for(let i = 0; i < this.streets.length; i++) {
-				const street = this.streets[i];
-				if(street.length > 1250) {
-					streets.push(street);
-				}
-			}
-			return streets;
-		};
-
-		this.getStreetFromName = function(name) {
-			for(let i = 0; i < this.streets.length; i++) {
-				if(this.streets[i].name === name) {
-					return this.streets[i];
-				}
-			}
-			throw 'Unable to find street with name [' + name + ']';
-		};
-	},
-
-	Street: function() {
-		this.city = Reviver.BACK_REFERENCE;
-		this.name;
-		this.length;
-		this.shops = [];
-
-		this.getHasMigros = function() {
-			for(let i = this.shops.length - 1; i >= 0; i--) {
-				if(this.shops[i].name === 'Migros') {
-					return true;
-				}
-			}
-			return false;
-		};
-	},
-
-	Shop: function() {
-		this.street = Reviver.BACK_REFERENCE;
-		this.name;
-		this.number;
-		this.category;
-
-		this.getLabel = function() {
-			return this.category + ' ' + this.name;
-		};
-
-		this.getAddress = function(language) {
-			return this.name + ' - ' + this.street.name + ', ' + this.number + ' - ' + this.street.city.names[language];
+class City {
+	static getProperties() {
+		return {
+			names: {type: 'object'},
+			population: {type: 'number'},
+			streets: {type: 'array'}
 		};
 	}
-};
+	constructor() {
+		//declaring properties here only helps type checking by a Typescript compiler
+		this.names = {};
+		this.population = 10;
+		/**@type {Array<Street>}*/
+		this.streets = [];
+	}
+
+	getLongStreets() {
+		return this.streets.filter(s => s.length > 1250);
+	}
+
+	getStreetFromName(name) {
+		const street = this.streets.find(s => s.name === name);
+		if(street) {
+			return street;
+		}
+		throw `Unable to find street with name [${name}]`;
+	}
+}
+
+class Street {
+	static getProperties() {
+		return {
+			city: {type: 'City', back_reference: true},
+			name: {type: 'string'},
+			length: {type: 'number'},
+			shops: {type: 'array'}
+		};
+	}
+	constructor() {
+		//declaring properties here only helps type checking by a Typescript compiler
+		/**@type {City}*/
+		this.city = undefined;
+		this.name = undefined;
+		this.length = undefined;
+		this.shops = [];
+	}
+
+	getHasMigros() {
+		return this.shops.some(s => s.name === 'Migros');
+	}
+}
+
+class Shop {
+	static getProperties() {
+		return {
+			street: {type: 'Street', back_reference: true},
+			name: {type: 'string'},
+			number: {type: 'number'},
+			category: {type: 'string'}
+		};
+	}
+	constructor() {
+		//declaring properties here only helps type checking by a Typescript compiler
+		/**@type {Street}*/
+		this.street = undefined;
+		this.name = undefined;
+		this.number = undefined;
+		this.category = undefined;
+	}
+
+	getLabel() {
+		return `${this.category} ${this.name}`;
+	}
+
+	getAddress(language) {
+		return `${this.name} - ${this.street.name}, ${this.number} - ${this.street.city.names[language]}`;
+	}
+}
+
+function buildEntity(entity) {
+	const builder = getConstructorForEntity(entity);
+	const instance = new builder();
+	/*for(const [property, value] of Object.entries(properties)) {
+		instance[property] = value;
+	}*/
+	return instance;
+}
 
 function getConstructorForEntity(entity) {
 	switch(entity) {
-		case 'City' : return TestEntities.City;
-		case 'Street' : return TestEntities.Street;
-		case 'Shop' : return TestEntities.Shop;
+		case 'City' : return City;
+		case 'Street' : return Street;
+		case 'Shop' : return Shop;
 	}
-	throw new Error('No constructor for entity ' + entity);
+	throw new Error(`No constructor for entity ${entity}`);
 }
 
 function getPropertiesForEntity(entity) {
-	switch(entity) {
-		case 'City' : return ['names', 'population', 'streets'];
-		case 'Street' : return ['city', 'name', 'length', 'shops'];
-		case 'Shop' : return ['street', 'name', 'number', 'category'];
-	}
-	throw new Error('No properties for entity ' + entity);
+	return getConstructorForEntity(entity).getProperties();
 }
 
 const city = {
@@ -84,31 +108,31 @@ const city = {
 		'de': 'Genf'
 	},
 	'population': 500000,
-	'entity': 'City',
+	'className': 'City',
 	'streets': [
 		{
 			'name': 'Avenue de Champel',
-			'entity': 'Street',
+			'className': 'Street',
 			'length': 1500,
 			'population': 100, //undeclared property
 			'shops': [
 				{
 					'name': 'Migros',
 					'number': 81,
-					'entity': 'Shop',
+					'className': 'Shop',
 					'category': 'Supermarket'
 				},
 				{
 					'name': 'Pompei',
 					'number': 75,
-					'entity': 'Shop',
+					'className': 'Shop',
 					'category': 'Restaurant'
 				}
 			]
 		},
 		{
 			'name': 'Rue de Lausanne',
-			'entity': 'Street',
+			'className': 'Street',
 			'length': 1000
 		}
 	]
@@ -117,111 +141,132 @@ const city = {
 const streets = [
 	{
 		'name': 'Avenue de Champel',
-		'entity': 'Street',
+		'className': 'Street',
 		'length': 1500,
 		'population': 100, //undeclared property
 		'shops': [
 			{
 				'name': 'Migros',
 				'number': 81,
-				'entity': 'Shop',
+				'className': 'Shop',
 				'category': 'Supermarket'
 			},
 			{
 				'name': 'Pompei',
 				'number': 75,
-				'entity': 'Shop',
+				'className': 'Shop',
 				'category': 'Restaurant'
 			}
 		]
 	},
 	{
 		'name': 'Rue de Lausanne',
-		'entity': 'Street',
+		'className': 'Street',
 		'length': 1000
 	}
 ];
+
+const street = {
+	'name': 'Avenue de Lausanne',
+	'className': 'Street',
+	'length': 'very long', //property that does not match its declared type
+	'population': 100, //undeclared property
+	'shops': []
+};
 
 const shops = {
 	Restaurant: {
 		'name': 'Pompei',
 		'number': 75,
-		'entity': 'Shop',
+		'className': 'Shop',
 		'category': 'Restaurant'
 	},
 	Supermarket: {
 		'name': 'Migros',
 		'number': 81,
-		'entity': 'Shop',
+		'className': 'Shop',
 		'category': 'Supermarket'
 	}
 };
 
 export default function test(assert) {
-	try {
-		assert.begin();
+	assert.begin();
 
-		let reviver;
+	let reviver;
 
-		//non strict mode, properties are not checked
-		reviver = new Reviver({
-			entityProperty: 'entity',
-			entitiesConstructors: getConstructorForEntity,
-			preserveEntityProperty: true
-		});
-		const revived_city_1 = reviver.revive(city);
-		assert.equal(revived_city_1.constructor, TestEntities.City, 'Constructor is the good class');
-		assert.equal(revived_city_1.names['en'], 'Geneva', 'Simple objects properties are not modified: city name in "en" is "Geneva"');
-		assert.equal(revived_city_1.population, 500000, 'Properties are not modified: population is 500000');
-		assert.equal(revived_city_1.streets[0].entity, 'Street', 'Entity property for first street is "Street" because preserveEntityProperty has been set to true');
-		assert.equal(revived_city_1.streets.length, 2, 'Arrays are not modified: there is 2 streets');
-		assert.equal(revived_city_1.getLongStreets().length, 1, 'Object has been revived and methods are available: there is 1 long street');
-		assert.equal(revived_city_1.streets[0].name, 'Avenue de Champel', 'Arrays and properties are not modified: first street name is "Avenue de Champel"');
-		assert.equal(revived_city_1.streets[0].population, 100, 'In non strict mode, even properties which have not been declared are copied: population for first street is 100');
-		assert.equal(revived_city_1.streets[0].city.names['fr'], 'Genève', 'Back referenced are managed: city name in "fr" for the first street is "Genève"');
-		assert.equal(revived_city_1.getStreetFromName('Avenue de Champel').shops[0].name, 'Migros', 'Methods are available and return revived objects: first shop in street "Avenue de Champel" is "Migros"');
-		assert.equal(revived_city_1.getStreetFromName('Avenue de Champel').shops[0].getAddress('de'), 'Migros - Avenue de Champel, 81 - Genf', 'Methods are available on revived objects: address in "de" for first shop in "Avenue de Champel" is "Migros - Avenue de Champel, 81 - Genf"');
+	//preserve unknown properties (properties are not checked)
+	reviver = new Reviver({
+		entityProperty: 'className',
+		entitiesConstructors: getConstructorForEntity,
+		entitiesProperties: getPropertiesForEntity,
+		preserveUnknownProperties: true,
+		preserveEntityProperty: true
+	});
+	const revived_city_1 = reviver.revive(city);
+	assert.equal(revived_city_1.constructor, City, 'Constructor is the good class');
+	assert.equal(revived_city_1.names['en'], 'Geneva', 'Simple objects properties are not modified: city name in "en" is "Geneva"');
+	assert.equal(revived_city_1.population, 500000, 'Properties are not modified: population is 500000');
+	assert.equal(revived_city_1.streets[0].className, 'Street', 'Entity property for first street is "Street" because preserveEntityProperty has been set to true');
+	assert.equal(revived_city_1.streets.length, 2, 'Arrays are not modified: there is 2 streets');
+	assert.equal(revived_city_1.getLongStreets().length, 1, 'Object has been revived and methods are available: there is 1 long street');
+	assert.equal(revived_city_1.streets[0].name, 'Avenue de Champel', 'Arrays and properties are not modified: first street name is "Avenue de Champel"');
+	assert.equal(revived_city_1.streets[0].population, 100, 'If preserve unknown properties is set to true, even properties that have not been declared are copied: population for first street is 100');
+	assert.equal(revived_city_1.streets[0].city.names['fr'], 'Genève', 'Back referenced are managed: city name in "fr" for the first street is "Genève"');
+	assert.equal(revived_city_1.getStreetFromName('Avenue de Champel').shops[0].name, 'Migros', 'Methods are available and return revived objects: first shop in street "Avenue de Champel" is "Migros"');
+	assert.equal(revived_city_1.getStreetFromName('Avenue de Champel').shops[0].getAddress('de'), 'Migros - Avenue de Champel, 81 - Genf', 'Methods are available on revived objects: address in "de" for first shop in "Avenue de Champel" is "Migros - Avenue de Champel, 81 - Genf"');
 
-		//strict mode, properties are checked and only declared properties are imported
-		reviver = new Reviver({
-			entityProperty: 'entity',
-			entitiesConstructors: getConstructorForEntity,
-			entitiesProperties: getPropertiesForEntity,
-			preserveEntityProperty: true,
-			debug: false
-		});
-		const revived_city_2 = reviver.revive(city);
-		assert.equal(revived_city_2.names['en'], 'Geneva', 'Simple objects properties are not modified: city name in "en" is "Geneva"');
-		assert.equal(revived_city_2.streets.entity, undefined, 'Entity property is undefined even if preserveEntityProperty has been set to true because entity property is not returned by entitiesProperties function');
-		assert.equal(revived_city_2.population, 500000, 'Properties are not modified: population is 500000');
-		assert.equal(revived_city_2.getLongStreets().length, 1, 'Object has been revived and methods are available: there is 1 long street');
-		assert.equal(revived_city_2.streets[0].population, undefined, 'In strict mode, properties which have not been declared are not copied: population for first street is undefined');
+	//do not preserve unknown properties (properties are checked and only declared properties are imported)
+	reviver = new Reviver({
+		entityProperty: 'className',
+		factory: buildEntity,
+		entitiesProperties: getPropertiesForEntity,
+		preserveEntityProperty: true,
+		debug: false
+	});
+	const revived_city_2 = reviver.revive(city);
+	assert.equal(revived_city_2.names['en'], 'Geneva', 'Simple objects properties are not modified: city name in "en" is "Geneva"');
+	assert.equal(revived_city_2.streets.className, undefined, 'Entity property is undefined even if preserveEntityProperty has been set to true because entity property is not returned by entitiesProperties function');
+	assert.equal(revived_city_2.population, 500000, 'Properties are not modified: population is 500000');
+	assert.equal(revived_city_2.getLongStreets().length, 1, 'Object has been revived and methods are available: there is 1 long street');
+	assert.equal(revived_city_2.streets[0].population, undefined, 'If preserve unknown properties is left default (set to false), properties that have not been declared are not copied: population for first street is undefined');
 
-		//array
-		const revived_streets = reviver.revive(streets);
-		assert.equal(revived_streets.length, 2, 'Array structure is preserved: there is two streets');
-		assert.equal(revived_streets[0].constructor, TestEntities.Street, 'Constructor is the good class');
-		assert.ok(revived_streets[0].getHasMigros(), 'Methods are available: first street contains a Migros');
-		assert.notOk(revived_streets[1].getHasMigros(), 'Methods are available: second street contains a Migros');
+	//array
+	const revived_streets = reviver.revive(streets);
+	assert.equal(revived_streets.length, 2, 'Array structure is preserved: there is two streets');
+	assert.equal(revived_streets[0].constructor, Street, 'Constructor is the good class');
+	assert.ok(revived_streets[0].getHasMigros(), 'Methods are available: first street contains a Migros');
+	assert.notOk(revived_streets[1].getHasMigros(), 'Methods are available: second street contains a Migros');
 
-		//map
-		const revived_shops = reviver.revive(shops);
-		assert.ok('Restaurant' in revived_shops, 'Map structure is preserved');
-		assert.ok('Supermarket' in revived_shops, 'Map structure is preserved');
-		assert.equal(revived_shops['Restaurant'].constructor, TestEntities.Shop, 'Constructor is the good class');
-		assert.doesThrow(
-			function() {
-				revived_shops['Supermarket'].getAddress('en');
-			},
-			undefined,
-			'Revived objects are self-contained'
-		);
-		assert.equal(revived_shops['Supermarket'].getLabel(), 'Supermarket Migros', 'Methods are available on revived objects: supermarket label is "Supermarket Migros"');
-		assert.equal(revived_shops.Restaurant.getLabel(), 'Restaurant Pompei', 'Methods are available on revived objects: restaurant label is "Restaurant Pompei"');
+	//map
+	const revived_shops = reviver.revive(shops);
+	assert.ok('Restaurant' in revived_shops, 'Map structure is preserved');
+	assert.ok('Supermarket' in revived_shops, 'Map structure is preserved');
+	assert.equal(revived_shops['Restaurant'].constructor, Shop, 'Constructor is the good class');
+	assert.doesThrow(
+		() => revived_shops['Supermarket'].getAddress('en'),
+		undefined,
+		'Revived objects are self-contained'
+	);
+	assert.equal(revived_shops['Supermarket'].getLabel(), 'Supermarket Migros', 'Methods are available on revived objects: supermarket label is "Supermarket Migros"');
+	assert.equal(revived_shops.Restaurant.getLabel(), 'Restaurant Pompei', 'Methods are available on revived objects: restaurant label is "Restaurant Pompei"');
 
-		assert.end();
-	}
-	catch(exception) {
-		assert.fail('Unexpected error with reviver ' + exception.message);
-	}
+	//property that does not match its declared type
+	reviver = new Reviver({
+		factory: buildEntity,
+		entitiesProperties: getPropertiesForEntity,
+	});
+	const revived_street = reviver.revive(street);
+	assert.equal(revived_street.length, 'very long', 'Properties that do not match their declared types are kept when types are not enforced');
+	reviver = new Reviver({
+		factory: buildEntity,
+		entitiesProperties: getPropertiesForEntity,
+		enforceTypes: true
+	});
+	assert.doesThrow(
+		() => reviver.revive(street),
+		undefined,
+		'Properties that do not match their declared types are discarded when types are enforced'
+	);
+
+	assert.end();
 }
