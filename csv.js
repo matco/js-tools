35cb417094df3be2081dde948cc4598941f64ad1
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2021-03-16 15:22:37 +0100

    Fix and improve CSV tool

--- csv.js
+++ csv.js
@@ -21,25 +21,28 @@ class CSV {
 		const filename = name || new Date().toFullDisplay();
 		const blob = this.toBlob();
 		const file = new File([blob], filename, {type: CSV.MIME_TYPE, lastModified: Date.now()});
-		const url = window.URL.createObjectURL(file);
+		const url = URL.createObjectURL(file);
 		//Chrome does not support to set location href
 		if(/Chrome/.test(navigator.userAgent)) {
 			const link = document.createFullElement('a', {href: url, download: filename});
+			//add link in the current document to be able to test the the download
+			//if the link is not included in the document, there is no way to detect if it has been "used" (created and programmatically clicked) in tests
+			document.body.appendChild(link); //this line is only for tests to be able to detect the click on the link
 			const event = document.createEvent('MouseEvents');
 			event.initEvent('click', true, true);
 			link.dispatchEvent(event);
+			//remove link because it is useless
+			link.remove();
 		}
 		else {
 			location.href = url;
 		}
 		//revoke url after event has been dispatched
-		setTimeout(function() {
-			window.URL.revokeObjectURL(url);
-		}, 0);
+		setTimeout(() => URL.revokeObjectURL(url), 0);
 	}
 }
 
 CSV.DECIMAL_SEPARATOR = 1.1.toLocaleString().substring(1, 2);
-CSV.MIME_TYPE = 'text/csv;charset=utf-8';
+CSV.MIME_TYPE = 'text/csv';
 
 export {CSV};
